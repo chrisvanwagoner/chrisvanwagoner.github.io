e20ae54897eb54722574fbda809b5979c5ac0995
Chris van Wagoner <chrisv@fastmail.com>
2022-02-22 21:17:54 -0500

    gulp dist

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -219,47 +219,39 @@ var app = new Vue({
 
 /* runthis.js */
 
-let body = document.querySelector('body');
-let preview = document.querySelector('#preview');
-let toggleButton = document.querySelector('.toggle');
-let fadeLeft = document.querySelectorAll('.fade-left');
-let fadeRight = document.querySelectorAll('.fade-right');
-let clientEntry = document.querySelectorAll('.client');
+let body = document.querySelector("body");
+let preview = document.querySelector("#preview");
+let toggleButton = document.querySelector(".toggle");
+let fadeLeft = document.querySelectorAll(".fade-left");
+let fadeRight = document.querySelectorAll(".fade-right");
+let clientEntry = document.querySelectorAll(".client");
 let windowHeight = window.innerHeight;
 let targetTop = windowHeight * 0.4;
 let targetBottom = windowHeight * 0.7;
-let pathMain = document.querySelector('.main__svg path');
-// let x, y = 0;
-// window.addEventListener('mousemove', e => {
-//     x = e.pageXOffset;
-//     y = e.pageYOffset;
+
+// let pathMain = document.querySelector(".main__svg path");
+// let pathMainAnime = anime({
+//     targets: pathMain,
+//     loop: false,
+//     direction: "linear",
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: "easeOutExpo",
+//     duration: 3000,
+//     autoplay: false,
+//     delay: 500,
 // });
-// // Print x and y to console on mousedown
-// window.addEventListener('mousedown', e => {
-// 	console.log(`x: ${x}\ty: ${y}`);
+
+// let pathFooter = document.querySelector(".footer__svg path");
+// let pathFooterAnime = anime({
+//     targets: pathFooter,
+//     loop: false,
+//     direction: "linear",
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: "easeOutExpo",
+//     duration: 4000,
+//     autoplay: false,
+//     delay: 500,
 // });
-let pathMainAnime = anime({
-	targets: pathMain,
-	loop: false,
-	direction: 'linear',
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: 'easeOutExpo',
-	duration: 3000,
-	autoplay: false,
-	delay: 500
-	
-});
-let pathFooter = document.querySelector('.footer__svg path');
-let pathFooterAnime = anime({
-	targets: pathFooter,
-	loop: false,
-	direction: 'linear',
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: 'easeOutExpo',
-	duration: 4000,
-	autoplay: false,
-	delay: 500
-});
 
 // // Updating #preview with client site images
 // clientEntry.forEach((el) => {
@@ -280,99 +272,99 @@ let pathFooterAnime = anime({
 // 	});
 // });
 
+// // Move header and client elements as page is scrolled
+// const scrollEvents = () => {
+//     fadeLeft.forEach((el) => {
+//         let top = el.getBoundingClientRect().top;
+//         let speed = el.dataset.speed;
+//         el.style.setProperty("--left", (top - el.offsetTop) * speed + "px");
+//         el.style.setProperty("--opacity", 1 - -top / 800);
+//     });
+//     fadeRight.forEach((el) => {
+//         let top = el.getBoundingClientRect().top;
+//         let speed = el.dataset.speed;
+//         el.style.setProperty("--left", (top - el.offsetTop) * speed + "px");
+//         el.style.setProperty("--opacity", 1 - -top / 800);
+//     });
 
+//     clientEntry.forEach((el) => {
+//         let distFromTop = el.getBoundingClientRect().top;
+//         let elName = el.querySelector(".client__name");
+//         let weight;
+//         let skew;
+//         let translateX;
+//         const maxWeight = 300;
+//         const maxSkew = 35;
+//         const maxTranslate = 100;
+//         if (distFromTop <= targetTop) {
+//             weight = (distFromTop / targetTop) * (maxWeight - 100) + 100;
+//             skew = ((targetTop - distFromTop) / targetTop) * maxSkew;
+//             translateX =
+//                 ((targetTop - distFromTop) / targetTop) ** 3 * maxTranslate;
+//             elName.style.setProperty("--wght", weight);
+//             elName.style.setProperty("--skew", `${skew.toString()}deg`);
+//             elName.style.setProperty(
+//                 "--translateX",
+//                 `-${translateX.toString()}px`
+//             );
+//         } else {
+//             elName.style.setProperty("--wght", maxWeight);
+//             elName.style.setProperty("--skew", "0");
+//             elName.style.setProperty("--translateX", "0");
+//         }
+//     });
+// };
 
-// Move header and client elements as page is scrolled
-const scrollEvents = () => {
+// const checkScrollMain = () => {
+//     let distFromTop = pathMain.getBoundingClientRect().top;
+//     if (distFromTop <= targetBottom) {
+//         window.removeEventListener("scroll", checkScrollMain);
+//         pathMainAnime.play();
+//     }
+// };
 
-	fadeLeft.forEach((el) => {
-		let top = el.getBoundingClientRect().top;
-		let speed = el.dataset.speed;
-		el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-		el.style.setProperty('--opacity', (1 - (-top / 800)));
-	});
-	fadeRight.forEach((el) => {
-		let top = el.getBoundingClientRect().top;
-		// let top = rect.top;
-		let speed = el.dataset.speed;
-		el.style.setProperty('--left', ((top - el.offsetTop) * speed) + 'px');
-		el.style.setProperty('--opacity', (1 - (-top / 800)));
-	});
+// const checkScrollFooter = () => {
+//     let distFromTop = pathFooter.getBoundingClientRect().top;
+//     if (distFromTop <= targetBottom) {
+//         window.removeEventListener("scroll", checkScrollFooter);
+//         pathFooterAnime.play();
+//     }
+// };
 
-	clientEntry.forEach((el) => {
-		let distFromTop = el.getBoundingClientRect().top;
-		let elName = el.querySelector('.client__name');
-		let weight;
-		let skew;
-		let translateX;
-		const maxWeight = 300;
-		const maxSkew = 35;
-		const maxTranslate = 100;
-		if (distFromTop <= targetTop) {
-			weight = ((distFromTop / targetTop) * (maxWeight - 100)) + 100;
-			skew = ((targetTop - distFromTop) / targetTop) * maxSkew;
-			translateX = (((targetTop - distFromTop) / targetTop) ** 3) * maxTranslate;
-			elName.style.setProperty('--wght', weight);
-			elName.style.setProperty('--skew', `${skew.toString()}deg`);
-			elName.style.setProperty('--translateX', `-${translateX.toString()}px`);
-		} else {
-			elName.style.setProperty('--wght', maxWeight);
-			elName.style.setProperty('--skew', '0');
-			elName.style.setProperty('--translateX', '0');
-		}
-	});
-
-}
-
-const checkScrollMain = () => {
-	let distFromTop = pathMain.getBoundingClientRect().top;
-	if (distFromTop <= targetBottom) {
-		window.removeEventListener('scroll', checkScrollMain);
-		pathMainAnime.play();
-	}
-};
-
-const checkScrollFooter = () => {
-	let distFromTop = pathFooter.getBoundingClientRect().top;
-	if (distFromTop <= targetBottom) {
-		window.removeEventListener('scroll', checkScrollFooter);
-		pathFooterAnime.play();
-	}
-};
-
-window.addEventListener('scroll', checkScrollMain);
-window.addEventListener('scroll', checkScrollFooter);
-window.addEventListener('scroll', () => {
-	window.requestAnimationFrame(scrollEvents);
-});
-window.addEventListener('resize', () => {
-	window.requestAnimationFrame(scrollEvents);
-});
-
+// window.addEventListener("scroll", checkScrollMain);
+// window.addEventListener("scroll", checkScrollFooter);
+// window.addEventListener("scroll", () => {
+//     window.requestAnimationFrame(scrollEvents);
+// });
+// window.addEventListener("resize", () => {
+//     window.requestAnimationFrame(scrollEvents);
+// });
 
 // Toggle dark/light mode
 let toggleMode = () => {
-	if (body.classList.contains('dark-mode') == true) {
-		body.classList.add('light-mode');
-		body.classList.remove('dark-mode');
-		event.target.innerHTML = '<span id="toggle-label" hidden>Switch to dark mode</span>';
-	} else {
-		body.classList.add('dark-mode');
-		body.classList.remove('light-mode');
-		event.target.innerHTML = '<span id="toggle-label" hidden>Switch to light mode</span>';
-	}
+    if (body.classList.contains("dark-mode") == true) {
+        body.classList.add("light-mode");
+        body.classList.remove("dark-mode");
+        event.target.innerHTML =
+            '<span id="toggle-label" hidden>Switch to dark mode</span>';
+    } else {
+        body.classList.add("dark-mode");
+        body.classList.remove("light-mode");
+        event.target.innerHTML =
+            '<span id="toggle-label" hidden>Switch to light mode</span>';
+    }
 };
 
-toggleButton.addEventListener('click', toggleMode);
-let colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+toggleButton.addEventListener("click", toggleMode);
+let colorSchemeQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
-const setColorScheme = e => {
-	if (e.matches) {
-		body.classList.add('dark-mode');
-	} else {
-		body.classList.add('light-mode');
-	}
-}
+const setColorScheme = (e) => {
+    if (e.matches) {
+        body.classList.add("dark-mode");
+    } else {
+        body.classList.add("light-mode");
+    }
+};
 
 setColorScheme(colorSchemeQueryList);
 colorSchemeQueryList.addEventListener(setColorScheme, null);
